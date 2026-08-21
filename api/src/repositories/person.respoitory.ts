36c76037b9a65fe3cdb2ee5daa7ef3f4b7304af4
Asaph Arruda <asapharruda@gmail.com>
2026-08-21 11:11:20 -0300

    :rocket: wip: metodo de criacao no repository

---
+++ api/src/repositories/person.respoitory.ts
@@ -0,0 +1,20 @@
+import type { FindOptionsWhere, Repository } from "typeorm";
+import { Between, ILike } from "typeorm";
+import { Person } from "../models/person.model";
+import { AppDataSource } from "../configs/data-base";
+
+
+
+export default class PersonRepository {
+    private personRepository: Repository<Person>
+
+    constructor () {
+        this.personRepository = AppDataSource.getRepository(Person);
+    }
+
+    async create(person: Partial<Person>): Promise<Person> {
+        const newPerson = this.personRepository.create(person)
+        return await this.personRepository.save(newPerson)
+    }
+
+}
