f7c2ec2babbaba6ca6abcac9cdc800fa00e2daed
Asaph Arruda <asapharruda@gmail.com>
2026-08-21 11:12:33 -0300

    :chore: adiciona criacao do usuario junto com a pessoa

--- api/src/services/registration.service.ts
+++ api/src/services/registration.service.ts
@@ -1,16 +1,22 @@
 import { User } from "../models/user.model";
+import { Person } from "../models/person.model";
 import UserService, { UserService as UserServiceClass } from "./user.service";
+import PersonService, { PersonService as PersonServiceClass }from "./person.service";
 
 
 export class RegistrationService {
     constructor(
-        private readonly userService: UserServiceClass
+        private readonly userService: UserServiceClass,
+        private readonly personService: PersonServiceClass
     ) {}
-    async create(user: User, confirmPassword: string): Promise<User> {
+    async create(user: User, person: Person,confirmPassword: string): Promise<User> {
         user.validateForCreate();
+        person.validateForCreate();
         const newUser = await this.userService.create(user, confirmPassword);
+        const newPerson = await this.personService.create(newUser, person);
+        newUser.person = newPerson;
         return newUser;
     }
 }
 
-export default new RegistrationService(UserService);
+export default new RegistrationService(UserService,PersonService);
